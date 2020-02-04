1e17d3814205dd59241b6c5b48f8fd9e94bd7443
guerson9112 <maldonadodaniel92@gmail.com>
2020-02-04 12:27:38 -0500

    avance curso 4/2

--- notes-back/server/models/appuser.js
+++ notes-back/server/models/appuser.js
@@ -13,6 +13,39 @@ module.exports = function(Appuser) {
         next();
     });
 
+    Appuser.afterRemote('login', function(ctx, res, next){
+      var filter = {
+          "include" : ["superuser"]
+      }
+      Appuser.findById(ctx.result.userId, filter, function(err, result){
+        if(err == null){
+          var rJson = result.toJSON();
+          res.responseCode = 200;
+          if(rJson.emailVerified) {
+            res.emailVerified = true;
+
+          } else {
+            res.emailVerified = false;
+          }
+
+          res.verificationToken = rJson.verificationToken;
+          res.superuserId = rJson.superuserId;
+          if(typeof(rJson.role) !== "undefined" && rJson.role == 'owner'){
+            res.superuser = rJson.superuser;
+            res.role = rJson.role;
+          }
+          next();
+        }
+      })
+    });
+
+    Appuser.beforeRemote('create', function(context, user, next){
+      context.args.data.role = "owner";
+      next();
+    })
+    /**
+     * For user registration and sending verification email
+     */
     Appuser.beforeRemote('create', function( context, user, next){
       context.args.data.role = "owner";
       next();
@@ -112,4 +145,85 @@ module.exports = function(Appuser) {
         }
     })
     }
+
+
+    Appuser.remoteMethod('updateForgetPassword',
+      {
+        accepts: [{arg: 'user', type: 'object'}],
+        returns: {arg: 'resp', type: 'object'},
+        http: {path: '/updateForgetPassword', verb: 'post'}
+      })
+
+    Appuser.updateForgetPassword = function (user, cb) {
+      var uid = user.data.id;
+      var token = user.data.token;
+      var password = user.data.password;
+      Appuser.confirm(uid, token).then(function(response){
+        Appuser.findById(uid, function(err, user){
+          if(err){
+            cb(err, null);
+          }
+          if(user !== null && user !== undefined) {
+            user.password = password;
+            user.save(function(err){
+              if(err){
+                cb(err, null)
+              } else {
+                cb(null, {message: "Password  Updated Successfully", status : 200})
+              }
+            })
+          } else {
+            cb(null, {message: "User not found", status: 404})
+          }
+        })
+      }, function(err){
+        cb(err, null);
+      })
+     }
+
+    function generateVerificationToken(user, callback){
+      app.models.User.generateVerificationToken( user, {}, function(err, res){
+        if(err)
+          return callback(err, null)
+
+        callback(null, res)
+      })
+    }
+
+    Appuser.on('resetPasswordRequest', function(email, cb) {
+      Appuser.findById(email.user.id, function(err, result){
+          generateVerificationToken(result, function(error, token) {
+                result.verificationToken = token;
+                Appuser.upsert(result, function(err, user) {
+                  var encrypt_token = '' + user.verificationToken + '&uid=' + user.id;
+                  var verifyHref = 'http://localhost:4200/resetPassword' + '?token=' + encrypt_token;
+                  var redirectLink = 'http://localhost:4200/resetPassword';
+                  var message = {
+                    username: email.username,
+                    verifyHref: verifyHref
+                  }
+
+                  var renderer = loopback.template(path.resolve(__dirname, '../boot/views/forgetPassword.ejs'));
+                  var html = renderer(message);
+                  if(email.user) {
+                    var options = {
+                      type: 'email',
+                      to: email.email,
+                      from: 'guerson9112@gmail.com',
+                      subject: 'Reset Password Request',
+                      redirect: redirectLink,
+                      html: html
+                    }/* -- */
+
+                    Appuser.app.models.Email.send(options, function(err, response) {
+                      if(err) {
+                        return err;
+                      } /* -- */
+                    }); /* -- */
+                  } /* -- */
+                })/* -- */
+             })/* -- */
+          }) /* -- */
+      });
+
 };
